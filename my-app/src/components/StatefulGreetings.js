2aa51e7904275691c9d12a4ebe80ac36eaa9bbf4
AASanusi <alaminm.sanusi@gmail.com>
2023-01-31 22:30:06 +0000

    Add state to components

--- my-app/src/components/StatefulGreetings.js
+++ my-app/src/components/StatefulGreetings.js
@@ -1,10 +1,33 @@
 import React from "react";
 
 class StatefulGreetings extends React.Component {
-    render() {
-        return <h1>Hello {this.props.greeting} {this.props.name}</h1>
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            introduction: "Hello",
+            buttonText: "Exit",
+        };
     }
 
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText: "Enter"
+        }
+        );
+        console.log(this.state.introduction);
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1> {this.state.introduction} {this.props.greeting} </h1>
+                <button onClick={() => this.handleClick()}> {this.state.buttonText}</button>
+            </div>
+        )
+    }
 }
 
 export default StatefulGreetings
